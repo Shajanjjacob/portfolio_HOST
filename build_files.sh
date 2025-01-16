1eecc60679038826b532b1222a807caa56b22a0f
Shajan J Jacob <34437757+Shajanjjacob@users.noreply.github.com>
2025-01-17 02:41:48 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,3 @@
 # Build the project
-echo "Building the project..."
-Python 3.12.3 -m pip install -r requirements.txt
 
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
-
-echo "Migrating the database..."
-python3 manage.py migrate --noinput
+python manage.py collectstatic --noinput
