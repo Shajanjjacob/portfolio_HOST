c7679cced015f1477e2a67a16af4f1b89ff5c7c5
Shajan J Jacob <34437757+Shajanjjacob@users.noreply.github.com>
2025-01-17 02:31:17 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+Python 3.12.3 -m pip install -r requirements.txt
 
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+Python 3.12.3 manage.py makemigrations --noinput
+Python 3.12.3 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+Python 3.12.3 manage.py collectstatic --noinput --clear
